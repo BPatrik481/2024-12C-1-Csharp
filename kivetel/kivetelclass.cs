7fa71102cfceac427464a48d5d6c28bf95356098
Patrik András Budinszki <2021124@dszcberegszaszi.hu>
2024-11-27 10:07:39 +0100

    kivete

---
+++ kivetel/kivetelclass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivetel
+{
+    class kivetelclass
+    {
+        public kivetelclass() { }
+        //Általános kivételkezelés
+        public void KivKezOs()
+        {
+            Console.WriteLine("Nincs kivételezés\nAdjon meg egy karaktert!");
+            try
+            {
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám {0}", szam);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
