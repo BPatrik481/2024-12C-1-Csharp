09df6bac7cb116008d5915e217b54221bd92d672
Patrik András Budinszki <2021124@dszcberegszaszi.hu>
2025-02-19 07:47:35 +0100

    igen

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,11 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private bool iranyFel = true;
+        private bool iranyBalra = true;
+        private int lepesFel = 30;
+        private int lepesOldalt = 30;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,21 +26,39 @@ namespace WindowsFormsApp1
         {
 
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Location = new Point(225, 198);
-            button1.Text = "Katintittam";
-        }
+            Point point = button1.Location;
 
+            if (iranyFel && (point.Y - lepesFel) <= 0)
+            {
+                iranyFel = false;
+            }
+            if (iranyBalra && (point.X - lepesOldalt) <= 0)
+            {
+                iranyBalra = false;
+            }
+            if (!iranyFel && ((point.Y + button1.Height) + lepesFel) >= Height)
+            {
+                iranyFel = true;
+            }
+            if (!iranyBalra && ((point.X + button1.Width) + lepesOldalt) >= Width)
+            {
+                iranyBalra = true;
+            }
+
+            point.X += iranyBalra ? -lepesOldalt : lepesOldalt;
+            point.Y += iranyFel ? -lepesFel : lepesFel;
+            button1.Location = point;
+        }
         private void button1_MouseHover(object sender, EventArgs e)
         {
-            button1.BackColor = Color.AliceBlue;
+            button1.BackColor = Color.LightBlue;
         }
         private void button1_MouseLeave(object sender, EventArgs e)
         {
             button1.BackColor = Color.AntiqueWhite;
-            button1.Text = "Kattints";
+            button1.Text = "Kattints rám!";
         }
     }
 }
