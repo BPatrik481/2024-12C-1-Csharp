6d6bd8db77e19125aa638399f229996e5054a221
Patrik András Budinszki <2021124@dszcberegszaszi.hu>
2025-04-02 07:17:11 +0200

    asd

---
+++ kepekkorbe/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace kepekkorbe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.forgjobb = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.job = new System.Windows.Forms.Label();
+            this.forgatbal = new System.Windows.Forms.Label();
+            this.foragtj = new System.Windows.Forms.Label();
+            this.bal = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(509, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 74);
+            this.label6.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(365, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 74);
+            this.label5.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(468, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 74);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(262, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 74);
+            this.label3.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(212, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 74);
+            this.label10.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(468, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 74);
+            this.label9.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(262, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 74);
+            this.label7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(365, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 74);
+            this.label1.TabIndex = 0;
+            // 
+            // forgjobb
+            // 
+            this.forgjobb.Location = new System.Drawing.Point(551, 45);
+            this.forgjobb.Name = "forgjobb";
+            this.forgjobb.Size = new System.Drawing.Size(77, 76);
+            this.forgjobb.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(179, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 76);
+            this.label4.TabIndex = 11;
+            // 
+            // job
+            // 
+            this.job.Location = new System.Drawing.Point(551, 45);
+            this.job.Name = "job";
+            this.job.Size = new System.Drawing.Size(77, 76);
+            this.job.TabIndex = 7;
+            // 
+            // forgatbal
+            // 
+            this.forgatbal.Location = new System.Drawing.Point(179, 45);
+            this.forgatbal.Name = "forgatbal";
+            this.forgatbal.Size = new System.Drawing.Size(77, 76);
+            this.forgatbal.TabIndex = 11;
+            // 
+            // foragtj
+            // 
+            this.foragtj.Location = new System.Drawing.Point(551, 45);
+            this.foragtj.Name = "foragtj";
+            this.foragtj.Size = new System.Drawing.Size(77, 76);
+            this.foragtj.TabIndex = 7;
+            // 
+            // bal
+            // 
+            this.bal.Location = new System.Drawing.Point(179, 45);
+            this.bal.Name = "bal";
+            this.bal.Size = new System.Drawing.Size(77, 76);
+            this.bal.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(364, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 74);
+            this.label8.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label forgjobb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label job;
+        private System.Windows.Forms.Label forgatbal;
+        private System.Windows.Forms.Label foragtj;
+        private System.Windows.Forms.Label bal;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
